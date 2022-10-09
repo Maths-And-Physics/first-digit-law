641305a96a0977a0f64668c7e04fae72efc2e838
Stanley Masinde <stanmasinde@gmail.com>
2022-10-09 13:48:03 +0300

    chore add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,5 +6,15 @@
         <nuxt />
       </v-container>
     </v-main>
+
+    <v-footer>
+      <a
+        href="https://github.com/maths-and-physics/firs-digit-law"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Source code
+      </a>
+    </v-footer>
   </v-app>
 </template>
